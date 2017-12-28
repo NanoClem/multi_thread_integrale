9bde9e9192445cb015c3a89ba5c9ebe326a53d1b
NanoClem <root@Clment-PC.localdomain>
2017-12-28 01:12:19 +0100

    ajout de traçage automatique des courbe d'exécution (pas fini)

--- integrale_thread.cpp
+++ integrale_thread.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <cstdlib>
 #include <pthread.h>
 #include <mutex>
 #include <math.h>
-#include <time.h>
+#include <chrono>
 
 using namespace std;
-
-
-#define NBITERATION 1000
-#define NBTHREADS 2
 
 
 // Afficher les infos relatives à la mémoire
@@ -20,13 +17,58 @@ free -t\
 "
 
 
+// TEST DE L'EXISTENCE DU FICHIER CONTENANT LES RESULTATS DU CALCUL
+bool doExists(string filename)
+{
+  ifstream ifs(filename.c_str(), ios::in);
+  if(ifs)
+    return true;
+  else
+    return false;
+}
+
+
+// CREATION DU FICHIER DES RESULTATS
+// avec une base définie
+void createFile(string filename)
+{
+  ofstream ofs(filename.c_str(), ios::out);
+  if(ofs)
+  {
+    cout << "fichier cree avec succes \n";
+    ofs.close();
+  }
+  else
+    throw runtime_error("Erreur : impossible de creer le fichier !");
+}
+
+
+// ECRIRE LES DONNEES DANS LE FICHIER DES RESULTATS
+void writeFileData(string filename, const unsigned int ITERATION, long double execTime)
+{
+  ofstream ofs;
+  ofs.open(filename.c_str(), ios::out | ios::app);          // on écrit à la fin du fichier avec "app" pour éviter l'overwrite
+  if(ofs)
+  {
+    ofs << ITERATION
+        << "   "
+        << execTime
+        << "\n";
+
+    ofs.close();
+  }
+  else
+    throw runtime_error("Erreur : impossible d'ecrire dans le fichier !");
+}
+
+
 //MUTEX : verrou pour les variables partagées et les zones critiques
 mutex mtx;
 
 
 //VARIABLES GLOBALES
 long double temp_integral = 0;					      // Valeur finale de l'intégrale
-float partSize = NBITERATION  / NBTHREADS;		// Nombre de trapèzes calculés par thread
+
 
 
 
@@ -35,9 +77,10 @@ float partSize = NBITERATION  / NBTHREADS;		// Nombre de trapèzes calculés par
 
 typedef struct
 {
-  long double min, max;		// Bornes minorante et majorante de la fonction
-  double coef[3];			    // Coefficients du polynôme
-  float bmin, bmax;       // Intervalle sur lequel le calcul de l'intégrale est effectué
+  long double min, max;		          // Bornes minorante et majorante de la fonction
+  double coef[3];			              // Coefficients du polynôme
+  float bmin, bmax;                 // Intervalle sur lequel le calcul de l'intégrale est effectué
+  int ITERATION;              // Nombre d'itérations de la fonction
 }Function;
 
 
@@ -90,12 +133,13 @@ void * Ttrapeze(void * _args)
   long double xi, xj;
   long double a = _p->min;
   long double b = _p->max;
+  const int N = _p->ITERATION;
 
 
   for(size_t i=_p->bmin; i < size_t(_p->bmax); ++i)
   {
-      xi = a+(b-a)*i / double(NBITERATION);               // On recalcule les bornes actuelles
-      xj = a+(b-a)*(i+1) / double(NBITERATION);           // pour faire le calcul jusqu'à bmax
+      xi = a+(b-a)*i / double(N);               // On recalcule les bornes actuelles
+      xj = a+(b-a)*(i+1) / double(N);           // pour faire le calcul jusqu'à bmax
       T  = (xj-xi)/2.0f * (fn(xi,_p)+fn(xj,_p));
 
       if(T<0 || a>b)
@@ -115,9 +159,22 @@ void * Ttrapeze(void * _args)
 
 //FONCTION MAIN
 
-int main()
+int main(int argc, char * argv[])
 {
   printf("\n");
+
+  // PARAMETRES DE CONSOLE
+  if(argc != 3)                                                         // argv[0] contient le nom de l'exécutable
+  {
+    cout << "Nombre d'arguments invalide, veuillez en renseigner le nombre de threads PUIS le nombre d'iteration \n";
+    cout << "\n";
+    exit(0);
+  }
+
+  const unsigned int NBTHREADS = atoi(argv[1]);                    // Nombre de threads, atoi permet de convettir argv[] en entier
+  const int NBITERATION        = atoi(argv[2]);                    // Nombre d'iteration
+  float partSize               = NBITERATION  / NBTHREADS;		     // Nombre de trapèzes calculés par thread
+
 
 
   // MEMOIRE AVANT LANCEMENT
@@ -127,7 +184,6 @@ int main()
 
   // VARIABLES
   pthread_t thtab[NBTHREADS];
-  clock_t begin, end;
   Function * myFunctions = new Function[NBTHREADS];
 
 
@@ -138,13 +194,14 @@ int main()
 
   for(size_t i=0; i<NBTHREADS; ++i)
   {
-    myFunctions[i] . min = 0;                   // intervalle global de la fonction
-    myFunctions[i] . max = 5;                   // commun à tous les threads
-    myFunctions[i] . bmin = partSize*i;         // on repousse les bornes min et max locales de chaque structure pour avoir le bon intervalle de calcul
-    myFunctions[i] . bmax = partSize*(i+1);     // pour chaque thread et ainsi éviter de calculer sur le même intervalle
-    myFunctions[i] . coef[0] = 5;
-    myFunctions[i] . coef[1] = 2;
-    myFunctions[i] . coef[2] = 20;
+    myFunctions[i] . min       = 0;                  // intervalle global de la fonction
+    myFunctions[i] . max       = 5;                  // commun à tous les threads
+    myFunctions[i] . bmin      = partSize*i;         // on repousse les bornes min et max locales de chaque structure pour avoir le bon intervalle de calcul
+    myFunctions[i] . bmax      = partSize*(i+1);     // pour chaque thread et ainsi éviter de calculer sur le même intervalle
+    myFunctions[i] . coef[0]   = 5;
+    myFunctions[i] . coef[1]   = 2;
+    myFunctions[i] . coef[2]   = 20;
+    myFunctions[i] . ITERATION = NBITERATION;
   }
 
 
@@ -155,7 +212,7 @@ int main()
 
 
   //DEPART DU COMPTEUR
-  begin = clock();
+  chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
 
   //DEPART DES THREADS
@@ -187,6 +244,12 @@ int main()
   }
 
 
+  //FIN DU COMPTEUR
+  chrono::steady_clock::time_point end = chrono::steady_clock::now();
+  chrono::duration<long double> time_span = chrono::duration_cast<chrono::duration<long double>>(end-begin);
+  long double exec_time = time_span.count();
+
+
   delete [] myFunctions;
 
 
@@ -195,15 +258,30 @@ int main()
   printf("\n");
 
 
-  //FIN DU COMPTEUR
-  end = clock();
   printf("                                               ---------------------RESULTATS--------------------- \n");
   printf("\n");
   printf("                                                 L'aire finale est de %Lf uA pour %d itération(s) \n", temp_integral, NBITERATION);
-  printf("                                                 Le programme s'est exécuté en %Lf secondes \n", (long double)(end-begin)/CLOCKS_PER_SEC);
+  printf("                                                 Le programme s'est exécuté en %Lf secondes \n", exec_time);
   printf("\n");
   printf("                                               --------------------------------------------------- \n");
   printf("\n");
+
+
+  //ECRITURE DES RESULTATS DANS UN FICHIER TXT
+  string filename = "resultats.txt";
+
+  if( doExists(filename) )
+    writeFileData(filename, NBITERATION, exec_time);
+  else
+  {
+    try {
+      createFile(filename);
+      writeFileData(filename, NBITERATION, exec_time);
+    }
+    catch(exception& e) {
+      cout << e.what() << endl;
+    }
+  }
 
 
   return(EXIT_SUCCESS);
